02cd85c0dd8a34b0aeb4daf0e7c9bb4ba645049b
Peter Kovacs <peter@sanebox.com>
2022-12-06 07:25:42 -0500

    Day 6

--- Sources/AdventOfCode/Day6.swift
+++ Sources/AdventOfCode/Day6.swift
@@ -1,6 +1,15 @@
 import ArgumentParser
+import Algorithms
 
 struct Day6: ParsableCommand {
+    @Argument var count = 4
+    
     func run() {
+        let input = allInput
+        let signal = input.windows(ofCount: count).enumerated().first { a in
+            Set(a.element).count == count
+        }!.offset + count
+        
+        print("signal @", signal)
     }
 }
