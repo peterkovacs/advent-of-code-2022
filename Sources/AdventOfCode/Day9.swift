5bb417cd068ff4520c6151b9cc04ac00926a7e91
Peter Kovacs <peter@sanebox.com>
2022-12-09 16:52:37 -0500

    Day 9

--- Sources/AdventOfCode/Day9.swift
+++ Sources/AdventOfCode/Day9.swift
@@ -1,6 +1,81 @@
 import ArgumentParser
+import Parsing
+
+fileprivate var parser = Many {
+    OneOf {
+        Parse { "D "; Int.parser() }.map { (\Coordinate.down, $0) }
+        Parse { "U "; Int.parser() }.map { (\Coordinate.up, $0) }
+        Parse { "L "; Int.parser() }.map { (\Coordinate.left, $0) }
+        Parse { "R "; Int.parser() }.map { (\Coordinate.right, $0) }
+    }
+} separator: {
+    Whitespace(.vertical)
+} terminator: {
+    Whitespace(.vertical)
+    End()
+}
+
+fileprivate let translations = [
+    Coordinate(x: -2, y:  0): Coordinate(x: -1, y:  0),
+    Coordinate(x: -2, y: -1): Coordinate(x: -1, y: -1),
+    Coordinate(x: -2, y: -2): Coordinate(x: -1, y: -1),
+    Coordinate(x: -1, y: -2): Coordinate(x: -1, y: -1),
+    Coordinate(x:  0, y: -2): Coordinate(x:  0, y: -1),
+    Coordinate(x:  1, y: -2): Coordinate(x:  1, y: -1),
+    Coordinate(x:  2, y: -2): Coordinate(x:  1, y: -1),
+    Coordinate(x:  2, y: -1): Coordinate(x:  1, y: -1),
+    Coordinate(x:  2, y:  0): Coordinate(x:  1, y:  0),
+    Coordinate(x:  2, y:  1): Coordinate(x:  1, y:  1),
+    Coordinate(x:  2, y:  2): Coordinate(x:  1, y:  1),
+    Coordinate(x:  1, y:  2): Coordinate(x:  1, y:  1),
+    Coordinate(x:  0, y:  2): Coordinate(x:  0, y:  1),
+    Coordinate(x: -1, y:  2): Coordinate(x: -1, y:  1),
+    Coordinate(x: -2, y:  2): Coordinate(x: -1, y:  1),
+    Coordinate(x: -2, y:  1): Coordinate(x: -1, y:  1),
+]
 
 struct Day9: ParsableCommand {
-    func run() {
+    func part1(input: [(KeyPath<Coordinate, Coordinate>, Int)]) {
+        var grid = InfiniteGrid([true], minX: 0, minY: 0, maxX: 1, maxY: 1, defaultValue: false)
+        var head = Coordinate.zero
+        var tail = Coordinate.zero
+
+        for (direction, count) in input {
+            for _ in 0..<count {
+                defer { grid[tail] = true }
+                head = head[keyPath: direction]
+                if let translation = translations[head - tail] {
+                    tail = tail + translation
+                }
+            }
+        }
+        
+        print("part 1", grid.filter { $0 }.count)
+    }
+    
+    func part2(input: [(KeyPath<Coordinate, Coordinate>, Int)]) {
+        var grid = InfiniteGrid([false], minX: 0, minY: 0, maxX: 1, maxY: 1, defaultValue: false)
+        var knots = Array(repeating: Coordinate.zero, count: 10)
+        for (direction, count) in input {
+            for _ in 0..<count {
+                defer { grid[knots.last!] = true }
+                knots[0] = knots[0][keyPath: direction]
+                
+                for (head, tail) in zip(knots.indices, knots.indices.dropFirst()) {
+                    let (h, t) = (knots[head], knots[tail])
+                    if let translation = translations[ h - t ] {
+                        knots[tail] = t + translation
+                    } else { break }
+                }
+            }
+        }
+        
+        print("part 2", grid.filter { $0 }.count)
+    }
+    
+    func run() throws {
+        let input = try parser.parse(allInput)
+        part1(input: input)
+        part2(input: input)
     }
 }
---
+++ Sources/AdventOfCode/InfiniteGrid.swift
@@ -0,0 +1,118 @@
+//
+//  File.swift
+//  
+//
+//  Created by Peter Kovacs on 12/20/21.
+//
+
+import Foundation
+import Algorithms
+
+public struct InfiniteGrid<T>: Sequence {
+    public typealias Element = T
+    public private(set) var grid: [Coordinate: Element]
+    public private(set) var minX, minY, maxX, maxY: Int
+    let defaultValue: Element
+    
+    public init<S>(
+        _ s: S,
+        minX: Int = 0,
+        minY: Int = 0,
+        maxX: Int,
+        maxY: Int = 0,
+        defaultValue: Element
+    ) where S: Sequence, S.Element == Element
+    {
+        (self.minX, self.minY, self.maxX, self.maxY) = (minX, minY, maxX, maxY)
+        self.defaultValue = defaultValue
+        self.grid = .init(
+            uniqueKeysWithValues: zip(
+                CoordinateIterator(
+                    minX: minX,
+                    maxX: maxX,
+                    minY: minY,
+                    maxY: Int.max,
+                    coordinate: .init(x: minX, y: minY)
+                ),
+                s
+            )
+        )
+        if let max = grid.keys.max() {
+            setBounds( max )
+        }
+    }
+    
+    public subscript( _ c: Coordinate ) -> Element {
+        get {
+            return grid[c, default: defaultValue]
+        }
+        set {
+            setBounds(c)
+            grid[c] = newValue
+        }
+    }
+    
+    mutating public func setBounds(_ c: Coordinate) {
+        minX = Swift.min(minX, c.x)
+        maxX = Swift.max(maxX, c.x + 1)
+        minY = Swift.min(minY, c.y)
+        maxY = Swift.max(maxY, c.y + 1)
+    }
+    
+    public struct CoordinateIterator: IteratorProtocol, Sequence {
+        let minX, maxX, minY, maxY: Int
+        var coordinate: Coordinate
+        
+        public mutating func next() -> Coordinate? {
+            if !coordinate.isValid(minX: minX, x: maxX, minY: minY, y: maxY) {
+                coordinate = Coordinate(x: minX, y: coordinate.y + 1)
+            }
+            guard coordinate.isValid(minX: minX, x: maxX, minY: minY, y: maxY) else { return nil }
+            defer { coordinate = coordinate.right }
+
+            return coordinate
+        }
+    }
+    
+    public struct Iterator: IteratorProtocol {
+        let grid: InfiniteGrid
+        var iterator: CoordinateIterator
+        
+        public mutating func next() -> Element? {
+            guard let coordinate = iterator.next() else { return nil }
+            return grid[ coordinate ]
+        }
+    }
+    
+    public func makeIterator() -> Iterator {
+        return Iterator(
+            grid: self,
+            iterator: indices()
+        )
+    }
+    
+    public func indices() -> CoordinateIterator {
+        return CoordinateIterator(
+            minX: minX,
+            maxX: maxX,
+            minY: minY,
+            maxY: maxY,
+            coordinate: .init(x: minX, y: minY)
+        )
+    }
+
+}
+
+extension InfiniteGrid: CustomStringConvertible where Element: CustomStringConvertible {
+    public var description: String {
+        var result = ""
+        for (y, x) in product(minY..<maxY, minX..<maxX) {
+            result.append( self[.init(x: x, y: y)].description )
+
+            if x == maxX - 1 {
+                result.append("\n")
+            }
+        }
+        return result
+    }
+}
