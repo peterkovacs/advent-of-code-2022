4613d40d92d4a1bd3cf86b94997645ae9d80caa8
Peter Kovacs <peter@sanebox.com>
2022-12-15 16:49:05 -0500

    Day 15, cleanup & optimize

--- Sources/AdventOfCode/Day15.swift
+++ Sources/AdventOfCode/Day15.swift
@@ -20,43 +20,38 @@ fileprivate let parser = Parse {
     }
 }
 
-fileprivate extension Range where Bound: Comparable {
-    func intersection(with other: Self) -> Self {
-        .init(
-            uncheckedBounds: (
-                lower: Swift.max(lowerBound, other.lowerBound),
-                upper: Swift.min(upperBound, other.upperBound)
-            )
-        )
-    }
-}
+extension Coordinate {
+    func neighbors(atDistance distance: Int) -> [Coordinate] {
+        var x = self.go(in: \.up, distance)
+        var result = [x]
 
-fileprivate extension ArraySlice where Element == Range<Int> {
-    func alreadyIncluded(_ other: Range<Int>) -> Int {
-        guard let last = last else { return 0 }
-        let intersection = other.intersection(with: last)
-        
-        if !intersection.isEmpty {
-            return (
-                // If we're intersecting with a range, include it.
-                intersection.count -
-                // But don't include the range that is already included earlier.
-                dropLast().alreadyIncluded(intersection) +
-                // Recurse -- include the range that intersects with earlier commands.
-                dropLast().alreadyIncluded(other)
-            )
+        (1...distance).forEach { _ in
+            x = x.down.right
+            result.append(x)
+        }
+        (1...distance).forEach { _ in
+            x = x.down.left
+            result.append(x)
         }
         
-        return dropLast().alreadyIncluded(other)
+        (1...distance).forEach { _ in
+            x = x.up.left
+            result.append(x)
+        }
+        (1..<distance).forEach { _ in
+            x = x.up.right
+            result.append(x)
+        }
+        
+        return result
     }
 }
-
 
 struct Day15: ParsableCommand {
     @Option var Y: Int = 2000000
     @Option var range: Int = 4000000
     
-    func notClear(coordinates: [(Coordinate, Coordinate)], y: Int) -> Int {
+    func part1(coordinates: [(Coordinate, Coordinate)], y: Int) -> Int {
         // abs(x - sensor.x) + abs(y - sensor.y) <= sensor.distance, y = 2000000
         // abs(x - sensor.x) <= sensor.distance - abs(y - sensor.y)
         // eliminate anything with: sensor.distance - abs(y - sensor.y) < 0
@@ -64,7 +59,7 @@ struct Day15: ParsableCommand {
         
         let beacons = Set(coordinates.map(\.1).filter { $0.y == y })
         
-        return coordinates
+        let ranges: [Range<Int>] = coordinates
             .map { (sensor: $0.0, distance: $0.0.distance(to: $0.1)) }
             .filter { $0.distance - abs(y - $0.sensor.y) >= 0 }
             .map {
@@ -76,46 +71,44 @@ struct Day15: ParsableCommand {
                         )
                 )
             }
-            .reduce(into: (0, [Range<Int>]())) { result, range in
-                result.0 += range.count
-                result.0 -= result.1[...].alreadyIncluded(range)
-                result.1.append(range)
+            .sorted {
+                $0.lowerBound < $1.lowerBound
             }
-            .0 - beacons.count
-    }
-    
-    func part2(coordinates: [(Coordinate, Coordinate)]) -> Int {
-        for y in 0...range {
-            let ranges = coordinates
-                .map { (sensor: $0.0, distance: $0.0.distance(to: $0.1)) }
-                .filter { $0.distance - abs(y - $0.sensor.y) >= 0 }
-                .map {
-                    Range(
-                        uncheckedBounds:
-                            (
-                                lower: -($0.distance - abs(y - $0.sensor.y)) + $0.sensor.x,
-                                upper: ($0.distance - abs(y - $0.sensor.y)) + $0.sensor.x + 1
-                            )
-                    )
-                }
             
-            var x = 0
-            while x <= range {
-                guard let nextX = ranges.filter({ $0.contains(x) }).map(\.upperBound).max()
-                else {
-                    return x * 4000000 + y
-                }
-                
-                x = nextX
+        var count = ranges[0].count
+        var x = ranges[0].upperBound
+        for i in ranges.dropFirst() {
+            if i.contains(x) {
+                count += (x..<i.upperBound).count
+                x = i.upperBound
+            } else if x < i.lowerBound {
+                x = i.lowerBound
             }
         }
+
+        return count - beacons.count
+    }
+
+    func part2(coordinates: [(Coordinate, Coordinate)]) -> Int {
+        let distances = coordinates.map {
+            (sensor: $0.0, distance: $0.0.distance(to: $0.1))
+        }
         
-        fatalError()
+        let point = distances.lazy.flatMap {
+            $0.sensor.neighbors(atDistance: $0.distance + 1).filter {
+                $0.isValid(x: range, y: range)
+            }
+        }.first { coordinate in
+            distances.allSatisfy { $0.sensor.distance(to: coordinate) > $0.distance }
+        }!
+        
+        return point.x * 4000000 + point.y
     }
     
     func run() throws {
         let input = try stdin.map { try parser.parse($0) }
-        print("part 1", notClear(coordinates: input, y: Y))
+
+        print("part 1", part1(coordinates: input, y: Y))
         print("part 2", part2(coordinates: input))
     }
 }
